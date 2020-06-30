55fe2427066d9b0cbb8ce739af835311f0f55e4d
itayco <itayco@contel.co.il>
2020-06-30 15:25:59 +0300

    working game, need to fix the onmouseenter

---
+++ WindowsFormsApp1/Controller.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchMe
+{
+    public class Controller
+    {
+        private GameCatchMe m_GameForm;
+        private Random rnd = new Random();
+        public Controller()
+        {
+            m_GameForm = new GameCatchMe();
+            m_GameForm.m_GameController = this;
+        }
+
+        private void initialize()
+        {
+          
+        }
+
+        private int getClientXSize()
+        {
+            return m_GameForm.ClientSize.Width;
+        }
+
+        public void Run()
+        {
+            initialize();
+            m_GameForm.ShowDialog();
+        }
+
+        private int getClientYSize()
+        {
+            return m_GameForm.ClientSize.Height;
+        }
+
+        private int randNumberInLimits(int i_Limit)
+        {
+            return rnd.Next(i_Limit);
+        }
+
+        public void GetNewCordForButtonPos(int i_ButtonWidth, int i_ButtonHeight)
+        {
+            int newWidth = randNumberInLimits(getClientXSize());
+            int newHeight = randNumberInLimits(getClientYSize());
+             
+            if (newWidth > i_ButtonWidth)
+            {
+                newWidth -= i_ButtonWidth;
+            }
+            if (newHeight > i_ButtonHeight)
+            {
+                newHeight -= i_ButtonHeight;
+            }
+            m_GameForm.PlaceButton(newWidth, newHeight);
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/GameMainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchMe
+{
+    public partial class GameCatchMe : Form
+    {
+        public Controller m_GameController;
+        int m_CurrentScore;
+        Button currentButton;
+
+        public GameCatchMe()
+        {
+            InitializeComponent();
+            m_CurrentScore = 0;
+        }
+
+        
+
+        private void CatchMe_Click(object sender, EventArgs e)
+        {
+            m_CurrentScore++;
+            updateScore();
+            m_GameController.GetNewCordForButtonPos((sender as Button).Width, (sender as Button).Height);
+        }
+
+        private void updateScore()
+        {
+            this.Text = string.Format(@"CatchMe" + "  Score : {0}", m_CurrentScore);
+        }
+
+        public void PlaceButton(int i_NewWidth, int i_NewHeight)
+        {
+            currentButton.Top = i_NewHeight;
+            currentButton.Left = i_NewWidth;
+        }
+
+        private void CatchMe_MouseEnter(object sender, EventArgs e)
+        {
+            currentButton = sender as Button;
+            Point cursorPos = Cursor.Position;
+            bool checkForMargin = (cursorPos.X < currentButton.Left + currentButton.Width && cursorPos.X > currentButton.Left + currentButton.Width - 20);
+            checkForMargin = checkForMargin || ((cursorPos.X > currentButton.Left && cursorPos.X < currentButton.Left + 20));
+            if (!checkForMargin)
+            {
+                m_GameController.GetNewCordForButtonPos(currentButton.Width, currentButton.Height);
+            }
+        }
+    }
+}
